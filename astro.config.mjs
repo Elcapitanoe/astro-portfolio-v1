33ec3ca64ace3879e706418c416c9dee49ca21ab
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-10-03 15:28:40 +0700

    Add i18n support to sitemap in Astro config
    Configured the sitemap integration with i18n locales for multiple languages and updated the filter function for improved clarity. This enhances multilingual support for sitemap generation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
@@ -6,13 +5,33 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://domiadi.com',
-  trailingSlash: 'always', 
+  trailingSlash: 'always',
   integrations: [
     tailwind(),
     sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          ar: 'ar',
+          bn: 'bn',
+          en: 'en',
+          fr: 'fr',
+          hi: 'hi',
+          id: 'id',
+          pt: 'pt',
+          ru: 'ru',
+          es: 'es',
+          zh: 'zh', 
+        },
+      },
       
-      filter: (page) => page !== '/' && !page.includes('/draft'),
-      entryLimit: 45000,
+      filter: (page) => {
+        const { pathname } = new URL(page);
+        if (pathname === '/') return false;
+        if (pathname.includes('/draft')) return false;
+        return true;
+      },
+      entryLimit: 45000, 
     }),
     react(),
   ],
@@ -24,5 +43,4 @@ export default defineConfig({
       cssMinify: 'esbuild',
     },
   },
-  
 });
